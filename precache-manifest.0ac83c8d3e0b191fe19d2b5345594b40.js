9994971ad42c90342c47ef8570d705934f18ddf1
Neil Hughes <enhughesiasm@googlemail.com>
2020-05-08 15:05:00 +0100

    Updates

---
+++ precache-manifest.0ac83c8d3e0b191fe19d2b5345594b40.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c6cf7da00b9e18ca537aebb2300f6c",
+    "url": "/complex/index.html"
+  },
+  {
+    "revision": "0b819ee1cc9ca05b628d",
+    "url": "/complex/static/css/2.0c251c65.chunk.css"
+  },
+  {
+    "revision": "d3c176009b0ead853534",
+    "url": "/complex/static/css/main.e671b8cc.chunk.css"
+  },
+  {
+    "revision": "0b819ee1cc9ca05b628d",
+    "url": "/complex/static/js/2.b82df50c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/complex/static/js/2.b82df50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c176009b0ead853534",
+    "url": "/complex/static/js/main.b25cf2ac.chunk.js"
+  },
+  {
+    "revision": "1410fa44c5e0627edc4b",
+    "url": "/complex/static/js/runtime-main.fc516b8c.js"
+  },
+  {
+    "revision": "5e65188b00f456eeacbd0d09283482ab",
+    "url": "/complex/static/media/jar1transinverted.5e65188b.png"
+  },
+  {
+    "revision": "3d4ba374650070ed49e9dc0526c35691",
+    "url": "/complex/static/media/jarmix.3d4ba374.jpg"
+  },
+  {
+    "revision": "6ca742b41613d4dd380dcd96addf3e96",
+    "url": "/complex/static/media/shelves_bw.6ca742b4.jpg"
+  },
+  {
+    "revision": "8f4bb758be4afac102358fe0e430d5f0",
+    "url": "/complex/static/media/shop.8f4bb758.jpg"
+  }
+]);
